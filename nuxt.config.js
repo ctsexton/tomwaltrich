876991406518e72d9ef430b86dc998df32720b34
Cam Sexton <cameron.t.sexton@gmail.com>
2019-01-08 01:05:06 -0800

    create nuxt site

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'tomwaltrich',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  env: {
+    cockpit: {
+      apiUrl: 'http://tom.camsexton.com/api',
+      apiToken: '571bb3593a2cfb917e8a7f37ce2036',
+      baseUrl: 'http://tom.camsexton.com/'
+    }
+  },
+  modules: [
+    '@nuxtjs/axios'
+  ],
+  axios: {
+    proxyHeaders: false,
+    credentials: false
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        thomas waltrich
+      </h1>
+      <h2 class="subtitle">
+        {{ bio }}
+      </h2>
+      <div class="links">
+        <a
+          href=""
+          target="_blank"
+          class="button--green">Blooming</a>
+        <a
+          href=""
+          target="_blank"
+          class="button--grey">Ancient Animal Orchestra</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data () {
+  },
+  async asyncData({ $axios }) {
+    const apiUrl = process.env.cockpit.apiUrl
+    const token = process.env.cockpit.apiToken
+    const bio = await $axios.$get(`${apiUrl}/singletons/get/biography/biotext?token=${token}`);
+    return { bio }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 20px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
