16936f47e014b1b52c5147ae9d7fc4c4dbd201ff
Cam Sexton <cameron.t.sexton@gmail.com>
2019-01-08 01:16:23 -0800

    update font size

--- pages/index.vue
+++ pages/index.vue
@@ -49,7 +49,7 @@ export default {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 60px;
   color: #35495e;
   letter-spacing: 1px;
 }
