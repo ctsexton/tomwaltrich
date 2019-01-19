85887ddbb06d12d7cd67a87cf14722aac2dcb10a
Cam Sexton <cameron.t.sexton@gmail.com>
2019-01-18 16:47:26 -0800

    add contact form

---
+++ pages/contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-card flat width="100%" max-width="750px" class="pa-2">
+      <v-img :src="contact"></v-img>
+    </v-card>
+    <v-card flat width="100%" max-width="750px" class="pa-4">
+      <v-flex>
+        <span class="display-1">Contact Tom</span>
+        <v-form>
+          <v-text-field label="Name" required></v-text-field>
+          <v-text-field label="Email" required></v-text-field>
+          <v-textarea label="Message" required></v-textarea>
+          <v-btn left large color="purple">Submit</v-btn>
+        </v-form>
+      </v-flex>
+    </v-card>
+  </v-layout>
+</template>
+<script>
+export default {
+  computed: {
+    contact: function () {
+      const imgPath = this.$store.state.generalConfig.contact_form_image.path
+      const realPath = process.env.cockpit.realUrl
+      return `${realPath}${imgPath}`
+    }
+  }
+}
+</script>
---
+++ pages/video.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout column justify-start align-center>
+    <v-card-media :aspect-ratio="16/9" v-for="video in videos" :key="video.title" width="100%" max-width="900px" class="my-5 video-card">
+      <iframe :src="video.youtube_link" width="100%" height="100%" class="remove-border"></iframe>
+    </v-card-media>
+  </v-layout>
+</template>
+<script>
+export default {
+  computed: {
+    videos: function () { return this.$store.state.videos.entries }
+  }
+}
+</script>
+<style scoped>
+>>>.remove-border {
+  border: none;
+}
+>>>.video-card {
+  padding: 2%;
+  background: rgba(0,0,0,0.5);
+}
+</style>
